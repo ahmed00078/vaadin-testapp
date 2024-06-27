7843c7b80f594c52037321ce218a0b410470310a
start.vaadin.com <no-reply@vaadin.com>
2024-06-27 10:46:34 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/myview/MyViewView.java
@@ -0,0 +1,18 @@
+package com.example.application.views.myview;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("My View")
+@Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 2)
+@Route(value = "my-view")
+public class MyViewView extends Composite<VerticalLayout> {
+
+    public MyViewView() {
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+    }
+}
